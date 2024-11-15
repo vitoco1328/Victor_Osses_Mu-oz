4b607aaa03be0ea7f7c7bfc64b3079cebeea41d7
vitoco1328 <vosses1328@gmail.com>
2024-11-14 22:34:45 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+!DOCTYPE html>
+<html>
+
+    <head>
+        <title>control semana 7</title>
+    </head>
+    <fieldset>
+        </body>
+
+        <h1 style="color: forestgreen">Formulario de registro clientes</h1>
+        <p>Registro de clientes</p>
+        <input type="radio" name="botondeopcion" checked value="1">obligatorio<br />
+        <br>
+        <form action="index2.php" method="post">
+            <label for="username">Nombre de usuario:</label><br>
+            <input type="text" id="username" name=" username" required><br>
+            <br>
+            <label for="passwork">Contraseña:</label><br>
+            <input type="passwork" id="passwork" name="passwork" required><br>
+            <br>
+            <input type="submit" value="Registrar"><br>
+        </form>
+        <?php
+            
+            $username=$_POST["username"];
+            $passwork= $_POST["passwork"];
+            $username= "";
+            $passwork= "Vic#1428";
+            if (strlen($username)==8 && substr($username,-3)=="r12" ){ // se imprime si el usuario es valido o invalido.
+                echo "Usuario valido <br>" ;
+    
+            }
+            if (strlen($passwork)>= 8 && ctype_upper(substr($passwork,0,1)) && substr($passwork,3,1) =="*,#,%" ){
+                    //Se imprime si la contraseña es valida o invalida.
+                echo " Formato de contraseña valida <br>";
+        
+            }
+        >?
+            
+    </fieldset>
+
+</html>
