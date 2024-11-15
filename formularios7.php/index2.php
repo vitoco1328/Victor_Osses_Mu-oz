8737d3255d33315b1ae6d3945e0f5f3dbb338bbd
vitoco1328 <vosses1328@gmail.com>
2024-11-14 23:40:08 -0300

    Se agrega un archivo de código al repositorio

---
+++ formularios7.php/index2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>control semana 7</title>
+    </head>
+    <fieldset>
+        </body>
+        <h1 style="color: forestgreen">Formulario de registro clientes</h1>
+        <p>Registro de clientes</p>
+
+        <form action="formulario7.php" method="post">
+            <p>Nombre: <input type="text" name="nombre" /></p>
+            <p>Apellido:<input type="text" name="apellido" /></p>
+            <p> Rut: <input type="text" name="rut" /></p>
+            <p>Sexo:<input type="radio" name="sexo" value="Masculino">Masculino</p>
+            <p>Sexo:<input type="radio" name="sexo" value="femenino">Femenino</p>
+            <p>Direccion:<input type="text" name="direccion" /></p>
+            <p>E-mail: <input type="text" name="E-mail" /></p>
+            <p>Telefono: <input type="text" name="telefono" /></p>
+            <p>Marca: <input type="text" name="Marca"></p>
+            <p>Modelo: <input type="text" name="modelo"></p>
+            <p>Año: <input type="text" name="año"></p>
+
+            <p><input type="submit" value="registrar"></p>
+
+        </form>
+
+    </fieldset>
+
+</html>
